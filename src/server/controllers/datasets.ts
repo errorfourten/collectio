1c965cbb95f35b6807678a4e2e0b694345a998b6
errorfourten <yihhang@gmail.com>
2021-06-13 20:23:23 +0800

    Refactored structure of backend

--- src/server/controllers/datasets.ts
+++ src/server/controllers/datasets.ts
@@ -1,210 +1,17 @@
-import { Dataset, DatasetRawData } from '@util/types'
 import { RequestHandler } from 'express'
-import { v4 as uuid } from 'uuid'
+import service from '@services/datasets'
 import utils from './utils'
 
-let datasets: Array<Dataset> = [
-  {
-    id: 'ce01c3be-648a-4bc4-b6ad-62e8e84aa91e',
-    name: 'Very cool dataset',
-    dateCreated: new Date(2020, 11, 24, 10, 33, 30, 0),
-    project: 'basic',
-    description: 'This is the best dataset ever created',
-    notes: 'Need to take note of what is constitutes as cool',
-    attributes: [
-      {
-        name: 'size',
-        options: [
-          {
-            name: 'small',
-            quantity: 20
-          },
-          {
-            name: 'medium',
-            quantity: 60
-          },
-          {
-            name: 'large',
-            quantity: 20
-          }
-        ]
-      }, {
-        name: 'direction',
-        options: [
-          {
-            name: 'left',
-            quantity: 30
-          },
-          {
-            name: 'right',
-            quantity: 70
-          }
-        ]
-      }
-    ]
-  },
-  {
-    id: '459ed74c-0b1a-4db6-a852-c0dd602e55e7',
-    name: 'Another dataset',
-    dateCreated: new Date(2020, 12, 2, 20, 0, 0, 0),
-    project: 'basic',
-    description: 'Just another generic one',
-    attributes: [
-      {
-        name: 'size',
-        options: [
-          {
-            name: 'small',
-            quantity: 50
-          },
-          {
-            name: 'medium',
-            quantity: 20
-          },
-          {
-            name: 'large',
-            quantity: 30
-          }
-        ]
-      }, {
-        name: 'direction',
-        options: [
-          {
-            name: 'left',
-            quantity: 50
-          },
-          {
-            name: 'right',
-            quantity: 50
-          }
-        ]
-      }
-    ]
-  },
-  {
-    id: '2394ca32-8d0e-4e05-919b-6ffc83ffe804',
-    name: 'Big dataset',
-    dateCreated: new Date(2021, 1, 5, 0, 0, 0, 0),
-    project: 'basic',
-    notes: 'Gigantic dataset needs a lot of data',
-    attributes: [
-      {
-        name: 'size',
-        options: [
-          {
-            name: 'small',
-            quantity: 15
-          },
-          {
-            name: 'medium',
-            quantity: 45
-          },
-          {
-            name: 'large',
-            quantity: 40
-          }
-        ]
-      }, {
-        name: 'direction',
-        options: [
-          {
-            name: 'left',
-            quantity: 5
-          },
-          {
-            name: 'right',
-            quantity: 95
-          }
-        ]
-      }
-    ]
-  },
-  {
-    id: 'ec07515b-3202-47c5-9a4b-965fce23d44b',
-    name: 'A new beginning',
-    dateCreated: new Date(2020, 4, 2),
-    project: 'complex',
-    attributes: [
-      {
-        name: 'width',
-        options: [
-          {
-            name: 'thin',
-            quantity: 93
-          },
-          {
-            name: 'thick',
-            quantity: 302
-          }
-        ]
-      }, {
-        name: 'height',
-        options: [
-          {
-            name: 'small',
-            quantity: 39
-          },
-          {
-            name: 'medium',
-            quantity: 94
-          },
-          {
-            name: 'tall',
-            quantity: 23
-          }
-        ]
-      }
-    ]
-  },
-  {
-    id: '3c8bcb95-17ec-4f2e-b16f-d435fdaf0ddf',
-    name: 'This is the end',
-    dateCreated: new Date(2020, 5, 2),
-    project: 'complex',
-    description: 'Last ever dataset?',
-    attributes: [
-      {
-        name: 'width',
-        options: [
-          {
-            name: 'thin',
-            quantity: 32
-          },
-          {
-            name: 'thick',
-            quantity: 89
-          }
-        ]
-      }, {
-        name: 'height',
-        options: [
-          {
-            name: 'small',
-            quantity: 19
-          },
-          {
-            name: 'medium',
-            quantity: 203
-          },
-          {
-            name: 'tall',
-            quantity: 45
-          }
-        ]
-      }
-    ]
-  }
-]
-
 const getAll: RequestHandler = async (_req, res) => {
-  res.send(datasets)
+  const datasets = service.allDatasets()
+  res.json(datasets)
 }
 
 const getOne: RequestHandler = async (req, res) => {
   try {
     const id = utils.parseUUID(req.params.id)
-    const dataset = datasets.filter((d) => d.id === id)
-    if (dataset.length) {
+    const dataset = service.findDataset(id)
+    if (dataset) {
       res.json(dataset)
     } else {
       res.status(404).send('Dataset not found')
@@ -214,30 +21,11 @@ const getOne: RequestHandler = async (req, res) => {
   }
 }
 
-const addDataset = (dataset: DatasetRawData): Dataset => {
-  const newDataset = {
-    id: uuid(),
-    dateCreated: new Date(),
-    ...dataset
-  }
-
-  datasets.push(newDataset)
-  return newDataset
-}
-
-const removeDataset = (id: Dataset['id']) => {
-  datasets = datasets.filter((dataset) => dataset.id !== id)
-}
-
-const editDataset = (id: string, editedDataset: Dataset) => {
-  datasets = datasets.map((dataset) => (dataset.id === id ? editedDataset : dataset))
-}
-
 const create: RequestHandler = async (req, res) => {
   try {
     const newDataset = utils.toNewDataset(req.body)
-    const addedDataset = addDataset(newDataset)
-    res.json(addedDataset)
+    const addedDataset = service.addDataset(newDataset)
+    res.status(201).json(addedDataset)
   } catch (e) {
     res.status(400).send(e.message)
   }
@@ -246,7 +34,7 @@ const create: RequestHandler = async (req, res) => {
 const remove: RequestHandler = async (req, res) => {
   try {
     const id = utils.parseUUID(req.params.id)
-    removeDataset(id)
+    service.removeDataset(id)
     res.status(204).send()
   } catch (e) {
     res.status(400).send(e.message)
@@ -256,8 +44,12 @@ const remove: RequestHandler = async (req, res) => {
 const edit: RequestHandler = async (req, res) => {
   try {
     const id = utils.parseUUID(req.params.id)
+    if (!service.findDataset(id)) {
+      res.status(404).send('Dataset not found')
+    }
+
     const dataset = utils.toDataset(req.body)
-    editDataset(id, dataset)
+    service.editDataset(id, dataset)
     res.json(dataset)
   } catch (e) {
     res.status(400).send(e.message)
