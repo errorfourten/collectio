8ff31f228829b14a0c016b66517eaaaab203877e
errorfourten <yihhang@gmail.com>
2021-07-01 18:48:40 +0800

    Added mongoose

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,24 @@
 import routes from '@util/routes'
 import express from 'express'
 import errorMiddleware from '@middleware/errorMiddleware'
+import mongoose from 'mongoose'
+import common from '@util/common'
+
+mongoose
+  .connect(common.MONGODB_URI, {
+    useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true
+  })
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB', error.message)
+  })
+
+process.on('SIGINT', () => {
+  mongoose.connection.close()
+  console.log('Disconnected from MongoDB')
+})
 
 const app = express()
 
--- src/server/util/common.ts
+++ src/server/util/common.ts
@@ -2,7 +2,22 @@ import common from '@root/config/common'
 
 const PORT = process.env.PORT || 8000
 
+const getMongoDBURI = () => {
+  if (common.inProduction) {
+    if (!process.env.MONGO_PROD_DB) {
+      throw new Error('Production requires environment variable MONGO_PROD_DB to be set!')
+    }
+    return process.env.MONGO_PROD_DB
+  }
+  if (!process.env.MONGO_TEST_DB) {
+    throw new Error('Testing requires environment variable MONGO_TEST_DB to be set!')
+  }
+  return process.env.MONGO_TEST_DB
+}
+const MONGODB_URI = getMongoDBURI()
+
 export default {
   ...common,
-  PORT
+  PORT,
+  MONGODB_URI
 }
